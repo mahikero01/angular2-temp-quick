f6b38b810bddf91bb41430e670d492e5b246ae7c
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-06-12 06:36:52 +0800

    up

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
 }());
 AppComponent = __decorate([
     core_1.Component({
-        selector: 'my-app',
+        selector: 'app-root',
         template: "<h1>Hello {{name}}</h1>",
     })
 ], AppComponent);
