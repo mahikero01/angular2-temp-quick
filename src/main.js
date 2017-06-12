22c55b6ae34420b989db829f65d67836a9cb8e20
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-06-12 11:18:54 +0800

    up

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var app_module_1 = require("./app/app.module");
-//import { enableProdMode } from '@angular/core'  // un comment this for PROD
-//enableProdMode(); // un comment this for PROD
+var core_1 = require("@angular/core"); // un comment this for PROD
+core_1.enableProdMode(); // un comment this for PROD
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 //import { enableProdMode } from '@angular/core'  // un comment this for PROD
 
